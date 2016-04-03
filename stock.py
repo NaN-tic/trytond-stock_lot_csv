c58dc0d74c6b419d0b347022909732633c999859
resteve <resteve@zikzakmedia.com>
2016-04-03 09:05:33 +0200

    Add Python3 support about import StringIO

--- stock.py
+++ stock.py
@@ -1,7 +1,7 @@
 # This file is part of stock_lot_csv module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from StringIO import StringIO
+from io import StringIO
 from csv import reader
 from logging import getLogger
 from trytond.pool import Pool
